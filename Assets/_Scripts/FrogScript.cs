89bf6e92223328af6b7804a19a261bdbc9749cbd
Dylan Meszaros <divern@outlook.com>
2022-04-14 16:45:05 -0400

    Frog + Coin
    Coin currently doesn't work, but the asset is ready to be coded

---
+++ Assets/_Scripts/FrogScript.cs
@@ -0,0 +1,173 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogScript : MonoBehaviour
+{
+    private Vector3 startPosition;
+
+    public float speed;
+    public float radius;
+    [Range(0, 360)]
+    public float angle;
+
+    public float jumpForce = 100f;
+    public float groundCheckRadius = 0.15f;
+
+    [SerializeField] private Transform groundCheckPos;
+    [SerializeField] private LayerMask whatIsGround;
+
+    public GameObject player;
+
+    private Rigidbody2D rBody;
+    private Animator anim;
+    public bool isGrounded = false;
+
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+
+    public bool canSeePlayer;
+
+    private Vector3 lastPosition;
+    public bool isFacingLeft = true;
+    private float countTime = 0f;
+    private float waitTime = 100f;
+    private float direction = -1f;
+
+    void wait(float amountOfSeconds)
+    {
+        Invoke("CallMeWithWait", amountOfSeconds);
+    }
+
+    private void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        startPosition = transform.position;
+        lastPosition = startPosition;
+    }
+
+    private bool GroundCheck()
+    {
+        if (isGrounded == false && Physics2D.OverlapCircle(groundCheckPos.position, groundCheckRadius, whatIsGround) == true)
+        {
+            rBody.AddForce(new Vector2(-(direction * (jumpForce/3.5f)), -jumpForce));
+
+            //if (direction * jumpForce < 0)
+            //{
+            //    rBody.AddForce(new Vector2(direction * jumpForce, -jumpForce));
+            //}
+            //else
+            //{
+            //    rBody.AddForce(new Vector2(-(direction * jumpForce), -jumpForce));
+            //}
+        }
+        return Physics2D.OverlapCircle(groundCheckPos.position, groundCheckRadius, whatIsGround);
+    }
+
+    private void FixedUpdate()
+    {
+        countTime += 1f;
+        anim.SetFloat("Velocity", rBody.velocity.y);
+
+        isGrounded = GroundCheck();
+        anim.SetBool("isGrounded", isGrounded);
+        if (!isGrounded)
+        {
+            countTime = 0;
+        }
+        CheckFieldOfView();
+        if (canSeePlayer && isGrounded && countTime > waitTime)
+        {
+            ChasePlayer();
+        }
+        else if (!canSeePlayer && isGrounded && countTime > waitTime)
+        {
+            ReturnHome();
+
+        }
+    }
+
+    private void CheckFieldOfView()
+    {
+        Collider2D[] rangeChecks = Physics2D.OverlapCircleAll(transform.position, radius, targetMask);
+
+        if (rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.position, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics2D.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                {
+                    canSeePlayer = true;
+                }
+                else
+                {
+                    canSeePlayer = false;
+                }
+            }
+            else
+            {
+                canSeePlayer = false;
+            }
+        }
+        else if (canSeePlayer)
+        {
+            canSeePlayer = false;
+        }
+    }
+
+    private void Flip()
+    {
+        Vector3 temp = transform.localScale;
+        temp.x *= -1;
+        transform.localScale = temp;
+        isFacingLeft = !isFacingLeft;
+        direction *= -1f;
+    }
+
+    private void ReturnHome()
+    {
+        if (startPosition.x > transform.position.x && isFacingLeft == true)
+        {
+            //Frog needs to face right
+            Flip();
+        }
+        else if (startPosition.x < transform.position.x && isFacingLeft == false)
+        {
+            //Frog needs to face left
+            Flip();
+        }
+
+        Jump();
+    }
+
+    private void ChasePlayer()
+    {
+        if (player.transform.position.x > transform.position.x && isFacingLeft == true)
+        {
+            //Frog needs to face right
+            Flip();
+        }
+        else if (player.transform.position.x < transform.position.x && isFacingLeft == false)
+        {
+            //Frog needs to face left
+            Flip();
+        }
+
+        Jump();
+    }
+
+    private void Jump()
+    {
+        rBody.AddForce(new Vector2(direction * (jumpForce / 3.5f), jumpForce));
+        isGrounded = false;
+        //anim.SetTrigger("FrogJump");
+    }
+}
---
+++ Assets/_Scripts/GemScript.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemScript : MonoBehaviour
+{
+    private Rigidbody2D rBody;
+
+    private float countTime = 0f;
+    private float waitTime = 150f;
+    private float gravityScale = 0f;
+
+    private Vector2 maxPosition;
+    private Vector2 minPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxPosition = new Vector2(transform.position.x, transform.position.y + 0.1f);
+        minPosition = new Vector2(transform.position.x, transform.position.y - 0.1f);
+
+        rBody = GetComponent<Rigidbody2D>();
+        gravityScale = rBody.GetComponent<Rigidbody2D>().gravityScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countTime += 1f;
+
+        if (rBody.velocity.magnitude > 1)
+        {
+            rBody.velocity = Vector2.ClampMagnitude(rBody.velocity, 1);
+        }
+        else if (rBody.velocity.magnitude < -1)
+        {
+            rBody.velocity = Vector2.ClampMagnitude(rBody.velocity, -1);
+        }
+
+        if (transform.position.y >= maxPosition.y)
+        {
+            rBody.GetComponent<Rigidbody2D>().gravityScale = gravityScale;
+        }
+        else if (transform.position.y <= minPosition.y)
+        {
+            rBody.GetComponent<Rigidbody2D>().gravityScale = -gravityScale;
+        }
+
+        //if (countTime > waitTime)
+        //{
+        //    Flip();
+        //    countTime = 0f;
+        //}
+    }
+
+    //private void Flip()
+    //{
+    //    rBody.GetComponent<Rigidbody2D>().gravityScale *= -1;
+    //}
+}
