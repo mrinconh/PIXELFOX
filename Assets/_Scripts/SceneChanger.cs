50361137c0d18f37dbf5e8d3b60ec3a534a0d897
Fernanda <mrinconh@my.centennialcollege.ca>
2022-04-15 22:23:19 -0400

    Level transits, main menu

--- Assets/_Scripts/SceneChanger.cs
+++ Assets/_Scripts/SceneChanger.cs
@@ -5,11 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    
+    public void Level3()
+    {
+
+        SceneManager.LoadScene("Level 3");
+    }
+
+
+    public void Level2()
+    {
+
+        SceneManager.LoadScene("Level 2");
+    }
+
+
     public void Level1()
     {
 
-        SceneManager.LoadScene("level1");
+        SceneManager.LoadScene("Level1");
     }
 
 
